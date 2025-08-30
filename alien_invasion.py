05f2ae00b3a008d1693222624aa969d9941758e1
leoflo14 <terabrandptr@gmail.com>
2025-08-30 19:29:42 +0300

    add collision check for bullets and spawn new fleet if no aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -93,10 +93,20 @@ class AlienInvasion:
         """Обновляет позиции снарядов и уничтожает старые снаряды."""
         # Обновление позиций снарядов.
         self.bullets.update()
-        # Удаление снарядов, вышедших за край экрана.
+        # Уничтожение исчезнувших снарядов.
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+        self._check_bullet_alien_collisions()
+
+    def _check_bullet_alien_collisions(self):
+        """Обработка коллизий снарядов с пришельцами."""
+        # Удаление снарядов и пришельцев, участвующих в коллизиях.
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+        if not self.aliens:
+            # Уничтожение существующих снарядов и создание нового флота.
+            self.bullets.empty()
+            self._create_fleet()
 
     def _update_screen(self):
         """Обновляет изображения на экране и отображает новый экран."""
--- settings.py
+++ settings.py
@@ -12,9 +12,9 @@ class Settings:
         # Настройки корабля
         self.ship_speed = 1.5
         # Параметры снаряда
-        self.bullet_speed = 1
-        self.bullet_width = 3
-        self.bullet_height = 15
+        self.bullet_speed = 10
+        self.bullet_width = 5
+        self.bullet_height = 30
         self.bullet_color = (255, 255, 255)
         self.bullets_allowed = 50
         # Настройки пришельцев
